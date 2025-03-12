80aceef7c6491c28ccfdfbe064fcb47cc96f1b9a
Rahman Mubarok <rahman.mubarok@lup.events>
2025-03-12 14:35:15 +0700

    lint

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -56,7 +56,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-  } catch (error) {
+  } catch {
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
@@ -93,7 +93,7 @@ export async function updateInvoice(
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
-  } catch (error) {
+  } catch {
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
 
